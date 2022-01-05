50fac04cf39ebad677f8a71c11862a3db9207e59
legend3 <zhenguo50@126.com>
2022-01-05 20:39:28 +0800

    Initial commit

---
+++ src/main/java/org/legend/content/bean/Student.java
@@ -0,0 +1,42 @@
+package org.legend.content.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@Data
+public class Student {
+    private String name;
+    private Integer age;
+
+//    Student(String name, Integer age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+
+//    String getName() {
+//        return name;
+//    }
+//
+//    void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    Integer getAge() {
+//        return age;
+//    }
+//
+//    void setAge(Integer age) {
+//        this.age = age;
+//    }
+
+//    @Override
+//    public String toString() {
+//        return "Student{" +
+//                "name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+//    }
+}
